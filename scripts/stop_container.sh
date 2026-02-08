cb82b7caa6a5a8a6f3c334fd74ab59c56164d5fc
sandeep.tarte <sandeep.tarte@globant.com>
2026-02-08 12:36:33 +0530

    Fix port 8080 conflict by removing container

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-CONTAINER_NAME=hello-springboot
 # Stop the running container (if any)
 echo "Stopping the running container if any"
-docker rm -f $CONTAINER_NAME || true
+
+container_id=`docker ps | awk -F " " '{print $1}'`
+docker rm -f $container_id
