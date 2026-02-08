b23068d2e73fa8d42d1035c6d6747c8d6b81a60c
sandeep.tarte <sandeep.tarte@globant.com>
2026-02-08 11:23:23 +0530

    added logs

--- src/main/java/org/example/hello/HelloController.java
+++ src/main/java/org/example/hello/HelloController.java
@@ -1,12 +1,15 @@
 package org.example.hello;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
+        log.info("Inside hello controller");
         return "Hello from spring boot application from docker and aws pipeline";
     }
 }
